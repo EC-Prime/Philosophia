c78f05eb6532eb5d11cad02c5809301d4fa40ff1
Emmanuel <echuma792@gamil.com>
2026-08-29 10:41:31 +0200

    Add quotes array and adjust hover effect for previous quotes

---
+++ quotes.js
@@ -0,0 +1,10 @@
+const qoutes =[
+     {id: 1, text: `"You have power over your mind not outside events.
+     Realize this and you will find strength"`, author: "Marcus Aurelius"},
+
+     {id: 2, text: `"He who fears death will never do anything worthy of a man who is alive."`, author: "Seneca"},
+
+     {id: 3, text: `"Vanity of vanity is all vanities"`, author: "Ecclesiastes"},
+
+     {id: 4, text: `"Without music, life would be a mistake."`, author: "Nietzche"}
+]
