7d63458919ba402e7f30ccde295db89f13642f69
Emmanuel <echuma792@gmail.com>
2026-09-23 16:07:30 +0200

    Fix quote retrieval and update textarea ID for reflections

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 function getTodaysQuote(){
 
     //get today's day
-    const day = Math.floor(Date.now()/86400000);
+    const day = Math.floor(Date.now()/86400000)+10;
     let index = day % quotes.length;
     let today = quotes[index];
 
    return today;
 }
 
-getTodaysQuote();
+function renderTodayCard(quote){
+    let date = document.querySelector('.quote-block .date');
+    date.textContent = "TODAY, 27 AUGUST 2026";
+
+    let todayQuote = document.querySelector('.quote-block .quote');
+    todayQuote .textContent = quote.text;
+
+    let author = document.querySelector('.quote-block .author');
+    author.textContent = quote.author;
+}
+
+const quote = getTodaysQuote();
+renderTodayCard(quote);
--- quotes.js
+++ quotes.js
@@ -1,4 +1,4 @@
-const qoutes = [
+const quotes = [
   { id: 1, text: `You have power over your mind not outside events. Realize this and you will find strength`, author: "Marcus Aurelius" },
   { id: 2, text: `He who fears death will never do anything worthy of a man who is alive.`, author: "Seneca" },
   { id: 3, text: `Vanity of vanity is all vanities`, author: "Ecclesiastes" },
